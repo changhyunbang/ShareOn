8c637b086e6e84393af62492cf600042a5f00d1d
Changhyun <changhyun.bang@kt.com>
2018-09-07 10:20:04 +0900

    android project start

---
+++ app/src/main/java/com/rooms/kshare/shareon/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rooms.kshare.shareon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
